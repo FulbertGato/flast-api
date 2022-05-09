3ffd413ad727cb6c88e3666645eabe0a9a65dc9c
Gato Junior <dev@gatojunior.co>
2022-05-09 11:03:39 +0100

    test deploy branch

--- config/config.example.py
+++ config/config.example.py
@@ -4,5 +4,6 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 SESSION_TYPE = 'redis'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://doadmin:AVNS_IulpPOvonyP84Sy@test-python-api-db:25060/defaultdb'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost:3306/dbname'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
